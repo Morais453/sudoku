f683dec83abcf4ab7206a4bb7069e52ac4232393
Morais453 <danmorais453@hotmail.com>
2025-06-13 11:38:45 -0300

    delete main

--- src/tabuleiro/Main.java
+++
@@ -1,9 +0,0 @@
-package tabuleiro;
-
-public class Main {
-    public static void main(String[] args) {
-        Board board1 = new Board();
-        board1.startingGame();
-        board1.printBoard();
-    }
-}
